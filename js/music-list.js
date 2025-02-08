b1969914e0fbc456b08e9083bf97b3e69008ad76
Itz_Gabby <oshowobigabriel02@gmail.com>
2025-02-08 20:36:47 +0100

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [ 
+    {
+        name: "Fido - Awolowo",
+        artist: "Fido",
+        img: "Awolowo",
+        src: "Awolowo"
+    },
+    {
+        name: "Fido - Joy Is Coming",
+        artist: "Fido",
+        img: "Fido",
+        src: "Fido"
+    },
+    {
+        name: "Hyce ft BoyPee & Brown Joel - Constantly",
+        artist: "Hyce ft BoyPee & Brown Joel",
+        img: "Boy",
+        src: "Boy"
+    },
+    {
+        name: "Joeboy & Qing Madi - Adenuga",
+        artist: "Joeboy & Qing Madi",
+        img: "Joe",
+        src: "Joe"
+    },
+    {
+        name: "Davido ft Boy Joel & BrownPee - Ogechi",
+        artist: "Davido ft Brown Joel & BrownPee",
+        img: "Ogechi",
+        src: "Ogechi"
+    },
+    {
+        name: "Kizz Daniel - Marhaba",
+        artist: "Kizz Daniel",
+        img: "Kizz",
+        src: "Kizz"
+    },
+    {
+        name: "Khaid - Orobo",
+        artist: "Khaid",
+        img: "Orobo",
+        src: "Orobo"
+    },
+    {
+        name: "Ruger & Tiwa Savage - Toma Toma",
+        artist: "Ruger & Tiwa Savage",
+        img: "Tiwa",
+        src: "Tiwa"
+    },
+    {
+        name: "Burna Boy - Bundle by Bundle",
+        artist: "Burna Boy",
+        img: "Burna",
+        src: "Burna"
+    },
+    {
+        name: "Katseye - Touch",
+        artist: "Katseye",
+        img: "Touch",
+        src: "Touch"
+    },
+]
